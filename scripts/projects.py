f2da3b032ce159a1609d5905c8c035e3f399356a
freeman-lab <the.freeman.lab@gmail.com>
2021-09-07 21:19:12 -0700

    import fix and linting

--- scripts/projects.py
+++ scripts/projects.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+from functools import lru_cache
+
 import click
 import fsspec
 import geopandas
 import pandas as pd
-from functools import lru_cache
+import requests
 from shapely.geometry import box
 
 crs = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
@@ -28,11 +30,15 @@ simplification_fire = {
     'CAR1046': 250,
 }
 
+
 @lru_cache
 def load_project_db():
-    retro_uri = requests.get('https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/forest-offsets-database-v1.0.json')
+    retro_uri = requests.get(
+        'https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/forest-offsets-database-v1.0.json'
+    )
     retro_json = retro_uri.json()
     return retro_json
+
 
 def buffer_and_simplify(gdf, distance=None):
     gdf_out = gdf.copy(deep=True)
@@ -44,18 +50,17 @@ def buffer_and_simplify(gdf, distance=None):
 
 def load_nifc_fires():
     '''load nifc data for 2020/2021 fire season
-    
-    NB this is a bit of an undocumented NIFC feature -- the data supposedly only cover 2021 
-    but there are definitely 2020 fires included at the endpoint. 
+
+    NB this is a bit of an undocumented NIFC feature -- the data supposedly only cover 2021
+    but there are definitely 2020 fires included at the endpoint.
     This might not be true in the future.
-    
+
     https://data-nifc.opendata.arcgis.com/datasets/nifc::wfigs-wildland-fire-perimeters-full-history/about
     '''
     nifc_uri = 'https://storage.googleapis.com/carbonplan-data/raw/nifc/WFIGS_-_Wildland_Fire_Perimeters_Full_History.geojson'
     fires = geopandas.read_file(nifc_uri)
 
-    nifc_colnames = {'poly_IncidentName': 'name',
-                     'poly_Acres_AutoCalc': 'acres'}
+    nifc_colnames = {'poly_IncidentName': 'name', 'poly_Acres_AutoCalc': 'acres'}
     fires = fires.rename(columns=nifc_colnames)
 
     fires = fires[fires['irwin_FireDiscoveryDateTime'].str[:4].isin(['2020', '2021'])]
@@ -65,45 +70,50 @@ def load_nifc_fires():
         .apply(pd.Timestamp)
         .apply(lambda x: pd.Timestamp(x.date()))
     )
-    
+
     return fires.to_crs(crs)[['name', 'acres', 'ignite_at', 'geometry']]
+
 
 def load_mtbs_fires():
     '''
     load mtbs data
-    
+
     Originally from: https://www.mtbs.gov/direct-download
     '''
     fire_uri = 'https://storage.googleapis.com/carbonplan-data/raw/mtbs/mtbs_perimeter_data/mtbs_perims_DD.json'
     fires = geopandas.read_file(fire_uri)
 
     fires = fires[fires['Incid_Type'] == 'Wildfire']
-    
+
     mtbs_colnames = {'Incid_Name': 'name', 'BurnBndAc': 'acres'}
     fires = fires.rename(columns=mtbs_colnames)
-    
+
     fires['ignite_at'] = fires['Ig_Date'].apply(pd.Timestamp)
 
     return fires.to_crs(crs)[['name', 'acres', 'ignite_at', 'geometry']]
+
 
 def load_fires():
     nifc = load_nifc_fires()
     mtbs = load_mtbs_fires()
     return pd.concat([nifc, mtbs])
 
+
 def load_simple_project(project, distance=None):
     path = f'https://carbonplan.blob.core.windows.net/carbonplan-forests/offsets/database/projects/{project}/shape.json'
     gdf = geopandas.read_file(path).to_crs(crs)
     gdf = buffer_and_simplify(gdf, distance=distance)
     attributes = get_project_attributes(project)
-    for k,v in attributes.items():
+    for k, v in attributes.items():
         gdf[k] = v
     return gdf
 
-def get_project_attributes(opr_id): 
+
+def get_project_attributes(opr_id):
     retro_json = load_project_db()
     project = [project for project in retro_json if project['opr_id'] == opr_id][0]
     return {'project_start_date': project['rp_1']['start_date']}
+
 
 def make_project_fires(fires, project_shape, buffer=None, scale=1, distance=None):
     center = project_shape.centroid[0]
