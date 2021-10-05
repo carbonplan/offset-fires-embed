00549728abeee74810f0c33d78709f9bf9976106
Kata Martin <katasm@gmail.com>
2021-10-05 11:14:22 -0700

    Restructure tests for clarity + brevity

--- tests/pages.test.js
+++ tests/pages.test.js
@@ -1,8 +1,6 @@
 import puppeteer from 'puppeteer'
 
-const options = {
-  allowSizeMismatch: true,
-}
+const routes = ['/', '/embed/project-fires', '/embed/future-risk']
 const viewports = [
   { description: 'laptop', width: 1440, height: 1440 },
   { description: 'tablet', width: 768, height: 768 },
@@ -11,39 +9,24 @@ const viewports = [
 
 const baseUrl = process.env.TEST_BASE_URL || 'http://localhost:3000'
 
-viewports.forEach(({ description, height, width }) => {
-  describe(description, () => {
-    it('/', async () => {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.setViewport({ height, width })
-      await page.goto(baseUrl + '/')
-      await page.waitForNetworkIdle()
-      const image = await page.screenshot({ fullPage: true })
+routes.forEach((route) => {
+  describe(route, () => {
+    viewports.forEach(({ description, height, width }) => {
+      it(`renders at ${description}`, async () => {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({ height, width })
+        await page.goto(baseUrl + route)
+        await page.waitForNetworkIdle()
+        const image = await page.screenshot({ fullPage: true })
 
-      expect(image).toMatchImageSnapshot(options)
-    })
-
-    it('/embed/future-risk', async () => {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.setViewport({ height, width })
-      await page.goto(baseUrl + '/embed/future-risk')
-      await page.waitForNetworkIdle()
-      const image = await page.screenshot({ fullPage: true })
-
-      expect(image).toMatchImageSnapshot(options)
-    })
-
-    it('/embed/project-fires', async () => {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.setViewport({ height, width })
-      await page.goto(baseUrl + '/embed/project-fires')
-      await page.waitForNetworkIdle()
-      const image = await page.screenshot({ fullPage: true })
-
-      expect(image).toMatchImageSnapshot(options)
+        expect(image).toMatchImageSnapshot({
+          allowSizeMismatch: true,
+          customSnapshotIdentifier: `${
+            route === '/' ? 'demo' : route
+          }-${description}`,
+        })
+      })
     })
   })
 })
